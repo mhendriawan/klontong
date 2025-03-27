567a80b0a104b6b6582a3f0184ceae262e88fe1e
Muhammad Hendriawan <mhendriawan1@gmail.com>
2025-03-27 10:17:26 +0700

    feat: firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:klontong_app/app.dart';
 import 'package:klontong_app/utils/utils.dart';
 
 void main() async {
   await setupInjection();
+  await Firebase.initializeApp();
   runApp(const App());
 }
