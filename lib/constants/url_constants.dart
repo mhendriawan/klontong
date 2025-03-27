521bddcb8b57c0a323b562a55168a7f22eaf202d
Muhammad Hendriawan <mhendriawan1@gmail.com>
2025-03-27 14:04:42 +0700

    Update url_constants.dart

--- lib/constants/url_constants.dart
+++ lib/constants/url_constants.dart
@@ -1,16 +1,15 @@
 import 'package:klontong_app/utils/utils.dart';
+
 class UrlConstant {
   final String tokenApi;
   late final String baseUrl;
   late final String product;
 
-  // Private constructor
   UrlConstant._(this.tokenApi) {
     baseUrl = "https://crudcrud.com/api/$tokenApi";
     product = "$baseUrl/product";
   }
 
-  // Factory method to initialize with async token
   static Future<UrlConstant> create() async {
     String token = await RemoteConfig.fetchTokenApi();
     return UrlConstant._(token);
